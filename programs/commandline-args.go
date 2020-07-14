46ef33a699754cb5bd8fdca9f3fc6987fcb6a0dc
Mahakaal007 <abhishek.pes2016@gmail.com>
2020-07-14 22:44:50 +0530

    making the programs function global

--- programs/commandline-args.go
+++ programs/commandline-args.go
@@ -6,7 +6,7 @@ import (
 )
 
 // printCommandLineArgs prints the commandline args
-func printCommandLineArgs() {
+func PrintCommandLineArgs() {
 	for i := 0; i < len(os.Args); i++ {
 		fmt.Printf("%+v\n", os.Args[i])
 	}
